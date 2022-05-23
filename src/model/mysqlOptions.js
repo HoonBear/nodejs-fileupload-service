1c34a04b664204e67c4d8300238876abbffe4aaa
손정훈 <sonjeonghun@sonjeonghun-ui-MacBookPro.local>
2022-05-23 10:52:54 +0900

    [Fix] gitIgnore

--- src/model/mysqlOptions.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    host     : 'localhost',
-    port     : '3306',
-    user     : 'son',
-    password : 'sky09097!',
-    database : 'FILECLOUD',
-    multipleStatements : true,
-    charset : 'utf8mb4'
-}
